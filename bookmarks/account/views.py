8251e8c7f2680073b7d7ce9ac954fe1bfd8d54ea
Karl Behrens <karlbehrensg@gmail.com>
2020-10-31 19:28:11 -0300

    Extending the user model

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, Profile, \
+                   UserEditForm, ProfileEditForm
 
 
 def user_login(request):
@@ -44,6 +45,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user) 
             return render(
                 request,
                 'account/register_done.html',
@@ -56,3 +59,23 @@ def register(request):
         'account/register.html',
         {'user_form': user_form}
     )
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                  'profile_form': profile_form})
